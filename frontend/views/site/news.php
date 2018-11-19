c1b69b458a98ceb45b5f4f26de454b0cdf8b2cc7
Каир Жумадилов <k.zhumadilov@g.nsu.ru>
2018-11-19 15:33:57 +0700

    news

--- frontend/views/site/news.php
+++ frontend/views/site/news.php
@@ -4,13 +4,52 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->registerCssFile('css/news.css', ['depends' => ['yii\bootstrap\BootstrapPluginAsset']]);
+
+$this->title = Yii::t('app', 'News');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
+<div class="row">
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="item-new">
+            <div class="clearfix">
+                <div class="pull-left date">
+                    01.11.18
+                </div>
+            </div>
+            <h4><?= Yii::t('news', 'Russian-Belgian cooperation') ?></h4>
+            <p>
+                <?= Yii::t('news', 'In collaboration with the research team of the University of Ghent business trips, an information system was developed for the Petroglyphs of the Turu-Alty Kosh-Agachsky District, r. Altai.') ?>
+            </p>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="item-new">
+            <div class="clearfix">
+                <div class="pull-left date">
+                    15.10.18
+                </div>
+            </div>
+            <h4><?= Yii::t('news', 'Expedition to Chagan') ?></h4>
+            <p>
+                <?= Yii::t('news', 'The results of the expedition to Chagan, Kosh-Agach district, r. Altai in August 2018. 50 three-dimensional models of rock images were built, a terrain model based on aerial photography was assembled.') ?>
+            </p>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="item-new">
+            <div class="clearfix">
+                <div class="pull-left date">
+                    03.07.18
+                </div>
+            </div>
+            <h4><?= Yii::t('news', 'Project start') ?></h4>
+            <p>
+                <?= Yii::t('news', 'The project to develop an effective technology for documenting archaeological objects was financed by the Russian Science Foundation as part of the event “Research by research teams under the guidance of young scientists.” Grant issued for 3 years.') ?>
+            </p>
+        </div>
+    </div>
 </div>
+
