441c9751ae0d1b107449358bbd111ca7a10c2781
Каир Жумадилов <k.zhumadilov@g.nsu.ru>
2018-12-13 17:50:02 +0700

    images for news

--- frontend/views/site/news.php
+++ frontend/views/site/news.php
@@ -4,7 +4,7 @@
 
 use yii\helpers\Html;
 
-$this->registerCssFile('css/news.css', ['depends' => ['yii\bootstrap\BootstrapPluginAsset']]);
+$this->registerCssFile('css/news.css?201812131745', ['depends' => ['yii\bootstrap\BootstrapPluginAsset']]);
 
 $this->title = Yii::t('app', 'News');
 //$this->params['breadcrumbs'][] = $this->title;
@@ -20,6 +20,7 @@ $this->title = Yii::t('app', 'News');
                 </div>
             </div>
             <h4><?= Yii::t('news', 'Russian-Belgian cooperation') ?></h4>
+            <div style="background: url('/img/news/2018110101.png') center center no-repeat / cover" class="row poster"></div>
             <p>
                 <?= Yii::t('news', 'In collaboration with the research team of the University of Ghent, an information system was developed for the Petroglyphs of the Turu-Alty Kosh-Agachsky District, r. Altai.') ?>
             </p>
@@ -33,6 +34,7 @@ $this->title = Yii::t('app', 'News');
                 </div>
             </div>
             <h4><?= Yii::t('news', 'Expedition to Chagan') ?></h4>
+            <div style="background: url('/img/news/2018101501.jpg') center center no-repeat / cover" class="row poster"></div>
             <p>
                 <?= Yii::t('news', 'The results of the expedition to Chagan, Kosh-Agach district, r. Altai in August 2018. 50 three-dimensional models of rock images were built, a terrain model based on aerial photography was assembled.') ?>
             </p>
@@ -46,6 +48,7 @@ $this->title = Yii::t('app', 'News');
                 </div>
             </div>
             <h4><?= Yii::t('news', 'Project start') ?></h4>
+            <div style="background: url('/img/news/2018070301.jpg') center center no-repeat / cover" class="row poster"></div>
             <p>
                 <?= Yii::t('news', 'The project to develop an effective technology for documenting archaeological objects was financed by the Russian Science Foundation as part of the event “Research by research teams under the guidance of young scientists.” Grant issued for 3 years.') ?>
             </p>
