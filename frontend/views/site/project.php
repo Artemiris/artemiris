3ab53872445bf03b64f84da18e1b6f8dc1590bd8
Каир Жумадилов <k.zhumadilov@g.nsu.ru>
2018-12-27 17:10:23 +0700

    project

--- frontend/views/site/project.php
+++ frontend/views/site/project.php
@@ -10,64 +10,35 @@ $this->title = Yii::t('app', 'About');
 <h1><?= Html::encode($this->title) ?></h1>
 
 <p>
-    Одной из важнейших задач археологии является документирование и систематизация объектов историко-культурного
-    наследия. За прошедшие десятилетия были зафиксированы и, в том или ином качестве, описаны достаточно большие объемы
-    археологических данных, представленные в монографическом, каталогизированном видах. В последние годы технический
-    прогресс значительно повлиял на инструменты и возможности документации археологических объектов. При этом, несмотря
-    на значительные открывшиеся возможности, задача документации и систематизации археологических знаний обладает рядом
-    нерешенных проблем:
+    <?= Yii::t('project', 'Nowadays, one of the main problems of archaeology is documentation and systematization of historical and cultural objects. During the last decades sufficiently large amount of archaeological data were marked and described, the data were also presented in monographic or cataloged types. In recent years technical progress has significantly influenced the ways and possibilities of documentation of archaeological objects. At the same time, despite the new significant opportunities, the issue of documentation and systematization of archaeological knowledge has a number of unsolved problems:') ?>
 <ul>
     <li>
-        Во-первых, научные и общественно-значимые знания о культурном наследии: объектах археологического наследия,
-        отдельных археологических находках, землях и территориях историко-культурного значения, как правило,
-        представлены во фрагментированном виде. У множества научных групп сегодня есть свои базы данных, которые не
-        публикуются и не предоставляются для коллег. Обмен такими данными в настоящее время достаточно затруднен или
-        невозможен вообще.
+        <?= Yii::t('project', 'First, scientific and socially important knowledge of cultural heritage (the objects of the archaeological heritage, individual archaeological finds, lands and territories of historical and cultural significance) are predominantly presented in a fragmented form. Today many scientific groups have their own databases, which are not published or made available to colleagues. The exchange of such data is currently quite difficult or impossible at all.') ?>
     </li>
     <li>
-        Во-вторых, на сегодняшний момент отсутствует единообразный подход к описанию и атрибутированию археологических
-        объектов. Несмотря на существующие попытки в выработке общего стандарта, на сегодняшний день, в основном, каждая
-        научная группа пользуется своим стандартом.
+        <?= Yii::t('project', 'Secondly, nowadays there is no unified approach to the description and attribution of archaeological objects. Despite existing attempts to develop a common standard, each scientific group uses its own standard, basically.') ?>
     </li>
     <li>
-        В-третьих, можно констатировать некоторое отставание существующей археологической практики от технического
-        прогресса в области методов документирования археологических объектов.
+        <?= Yii::t('project', 'Thirdly, it is possible to mark a certain lag of the existing archaeological practice from the technical progress in the field of documentation of archaeological objects methods.') ?>
     </li>
     <li>
-        В-четвертых, не решен вопрос обеспечения доступа и обмена данными об уязвимых объектах. В результате этого,
-        сегодня, по возможности, все имеющиеся данные скрыты от общественности, при этом страдает качество и
-        эффективность научной коммуникации, как и научно-популяризаторские возможности.
+        <?= Yii::t('project', 'Fourthly, the issue of providing access and exchange of data on vulnerable objects has not been resolved. As a result, today, whenever possible, all available data is hidden from the public, while the quality and effectiveness of scientific communication, as well as popularization of science opportunities, suffer.') ?>
     </li>
 </ul>
 </p>
 
 <p>
-    Цель проекта – разработка эффективной технологии для документирования археологических объектов изобразительного ряда
-    (артефактов, памятников, комплексов памятников), обмена такими данными и, в допустимом объеме, публикации в открытом
-    доступе. В том числе, предполагается разработка единой археологической информационной системы и базы данных,
-    обеспечивающих возможность полноценного документирования петроглифов, археологических находок, археологических
-    участков, включая полноценное описание, атрибутирование объектов, 3D модели объектов. В рамках построения
-    информационной системы предполагается разработка методики картографирования с использованием ГИС-сервисов, создание
-    эффективной системы доступа для исследователей и широкой общественности и системы контроля авторства вводимых
-    материалов, реализация прототипов автоматизированных интеллектуальных инструментов поиска аналогий, закономерностей
-    и распознавания художественных сюжетов. Наполнение системы должно обеспечиваться не только за счет коллектива
-    авторов проекта, но и за счет прямой работы с системой сторонних специалистов, групп и организаций.
+    <?= Yii::t('project', 'The goal of the project is to develop an effective technology for documenting archaeological objects of the visual series (artifacts, sites, site complexes), sharing data and publishing it in open access (to an acceptable extent). In particular, it is supposed to develop a single archaeological information system and database, which are to document and store the petroglyphs, archaeological finds, archaeological sites, including a full description, attribution of the objects and 3D models of the objects. As a part of building an information system, it is supposed to develop a mapping technique using GIS services, create an effective access system for researchers and the general public and a control system of authorship of input materials, an implementation of prototypes of automated intelligent tools for searching analogies, patterns and recognition of artistic plots. Filling the system should be provided not only by the team of project authors, but also by direct work with the system of third-party specialists, groups and organizations.') ?>
 </p>
 
 <p>
-    В рамках проекта предполагается разработка оптимального алгоритмического обеспечения высокоточного документирования
-    и изучения археологических объектов в полевых условиях на основе стандартизации и автоматизации этапов работы
-    первичного документирования, в том числе с использованием оригинальных мобильных приложений.
+    <?= Yii::t('project', 'The project involves the development of optimal algorithmic support for high-precision documentation and study of archaeological objects in the field based on the standardization and automation of the stages of primary documentation, including the use of original mobile applications.') ?>
 </p>
 
 <p>
-    В исследовании предполагается: проведение анализа существующих методик 3D технологий документирования в
-    археологических полевых исследованиях и в музейной практике; разработка методических приемов обработки и
-    трансформации в 3D модель уникальных объектов разной степени детализации, в т.ч. со значительными утратами.
+    <?= Yii::t('project', 'The research assumes: an analysis of existing methods of 3D documentation technologies in archaeological field research and in museum practice; development of methods for processing and transformation of unique objects of varying degrees of detail (incl. with significant losses) into a 3D model.') ?>
 </p>
 
 <p>
-    Проект выполняется при поддержке программы «Проведение исследований научными группами под руководством молодых
-    ученых» Российского научного фонда. Грант № 18-78-10079 «Разработка технологий и информационной системы
-    документирования и научного обмена археологическими данными».
+    <?= Yii::t('project', 'The project is supported by “Research by scientific groups under the guidance of young scientists” program of the Russian Science Foundation. Grant No. 18-78-10079 "Development of technology and information systems for documenting and scientific exchange of archaeological data".') ?>
 </p>
